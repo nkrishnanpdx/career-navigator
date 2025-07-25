f38a6e8de5843a0bd3b9978ceebabf335e39f3ed
nkrishnan <35274686+nkrishnanpdx@users.noreply.github.com>
2025-07-24 20:11:06 -0700

    Create search_utils.py

---
+++ tools/search_utils.py
@@ -0,0 +1,23 @@
+import aiohttp
+from bs4 import BeautifulSoup
+
+async def fetch(session, url):
+    async with session.get(url) as response:
+        return await response.text()
+
+async def search_duckduckgo(query):
+    """Perform a simple DuckDuckGo search and return top result snippets"""
+    base_url = "https://html.duckduckgo.com/html/"
+    async with aiohttp.ClientSession() as session:
+        data = {'q': query}
+        async with session.post(base_url, data=data) as resp:
+            html = await resp.text()
+            soup = BeautifulSoup(html, "html.parser")
+            results = []
+            for a in soup.find_all("a", class_="result__a"):
+                title = a.get_text()
+                link = a.get("href")
+                results.append({"title": title, "link": link})
+                if len(results) >= 5:
+                    break
+            return results
